a582cf3065c8763ce3232613c01daaaa21b47437
Christopher96u <christophermenendezmorales@gmail.com>
2023-10-20 23:18:27 +1100

    add forgot module

---
+++ src/forgot/entities/forgot.entity.ts
@@ -0,0 +1,26 @@
+import { Column, CreateDateColumn, Entity, Index, ManyToOne, PrimaryGeneratedColumn, DeleteDateColumn, } from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+import { Allow } from 'class-validator';
+
+@Entity()
+export class Forgot {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Allow()
+  @Column()
+  @Index()
+  hash: string;
+
+  @Allow()
+  @ManyToOne(() => User, {
+    eager: true,
+  })
+  user: User;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
